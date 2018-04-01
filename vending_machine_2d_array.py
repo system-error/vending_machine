e7ebc04715cb802378bb8ac99cd4357f1c2f7dcb
Christos Dousmanopoulos <system-error@users.noreply.github.com>
2018-04-01 15:47:42 +0300

    Add files via upload

---
+++ vending_machine_2d_array.py
@@ -0,0 +1,25 @@
+coins = [[2,20],[1,20],[0.5,20],[0.2,40],[0.1,40],[0.05,100]]
+
+answer = 'y'
+while answer == 'y':
+	payment = float(input("Payment amount: "))
+	inserted = float(input("Inserted amount: "))
+
+	change = round(inserted - payment,2)
+	pos = 0
+	while change > 0:
+		print(coins[0][1])
+		print(change)
+		print(pos)
+		if coins[pos][0] > change or coins[pos][1] == 0:
+			if pos < len(coins) - 1:
+				pos = pos + 1
+			else:
+				print("Sorry, no more change")
+				break	
+			continue
+
+		print ("Give change ", coins[pos][0])
+		change = round(change - coins[pos][0])
+		coins[pos][1] = coins[pos][1] - 1
+	answer = input("Continue? ") 		
